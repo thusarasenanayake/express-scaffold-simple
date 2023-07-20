89f37556b5789c34dd6f487b1828039bed047d6e
Thusara Senanayake <89896736+thusarasenanayake@users.noreply.github.com>
2023-07-20 19:51:39 +0530

    feat: create a simple server

---
+++ server.js
@@ -0,0 +1,32 @@
+const { default: axios } = require("axios");
+var express = require("express");
+var logger = require("morgan");
+
+// ----- config -----
+const PORT = process.env.PORT || 5000;
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", async function (req, res,next) {
+    try {
+        const resp = await axios.get("https://reqres.in/api/users/1");
+        res.json(resp.data);
+    } catch (error) {
+        next(error)
+    }
+
+});
+
+app.use(function(err,req,res,next){
+    res.status(500).send(err)
+})
+
+app.listen(PORT, function () {
+    console.log(`app listening on port ${PORT}`)
+})
+
+
