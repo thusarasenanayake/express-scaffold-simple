abac6432e12f2c4a9b77e5c981c89fd1382944fa
Thusara Senanayake <89896736+thusarasenanayake@users.noreply.github.com>
2023-07-20 19:54:41 +0530

    chore: lint

--- server.js
+++ server.js
@@ -11,22 +11,19 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", async function (req, res,next) {
-    try {
-        const resp = await axios.get("https://reqres.in/api/users/1");
-        res.json(resp.data);
-    } catch (error) {
-        next(error)
-    }
-
+app.get("/", async function (req, res, next) {
+  try {
+    const resp = await axios.get("https://reqres.in/api/users/1");
+    res.json(resp.data);
+  } catch (error) {
+    next(error);
+  }
 });
 
-app.use(function(err,req,res,next){
-    res.status(500).send(err)
-})
+app.use(function (err, req, res, next) {
+  res.status(500).send(err);
+});
 
 app.listen(PORT, function () {
-    console.log(`app listening on port ${PORT}`)
-})
-
-
+  console.log(`app listening on port ${PORT}`);
+});
